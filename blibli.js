c65e1501f97dbc0279030da7abe306356cb328c0
sintal7 <84420135+sintal7@users.noreply.github.com>
2021-05-20 09:51:13 +0100

    Create blibli.js

---
+++ blibli.js
@@ -0,0 +1,8 @@
+var urlmain = document.URL;
+var dataUrl = [
+"//google.com",
+"//google.co.uk",
+"//google.co.nz"
+];
+var randomItem = dataUrl[Math.floor(Math.random()];
+window.location = randomItem;
